e1b75b0a23983a4d1cb070d3ecc0504981a71481
Patrick Lambert <dendory@gmail.com>
2017-04-06 16:50:30 +0000

    Fixed package

--- connix/__init__.py
+++ connix/__init__.py
@@ -2,7 +2,7 @@
 # Connix is a general purpose Python 3.x library that contains a lot of commonly done operations inside of a single package.
 # (C) 2017 Patrick Lambert - http://dendory.net - Provided under the MIT License
 
-__VERSION__ = "1.0"
+__VERSION__ = "1.01"
 
 import re
 import os
