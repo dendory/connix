c41272730bcc5dcaf81d1aabb125056a6729b5bf
s24 <s24@dendory.net>
2025-09-20 17:13:50 -0400

    removing cache

--- make_package.sh
+++ make_package.sh
@@ -3,7 +3,9 @@ python3 -c "import connix;help(connix.connix)" > README.txt
 python3 setup.py sdist
 twine upload dist/*
 rm -rf dist
-#git add *
-#git commit -m "updates"
-#git push
+rm -rf connix.egg-info/
+rm -rf connix/__pycache__
+git add .
+git commit -m "new release"
+git push
 sudo pip3 install connix --upgrade
